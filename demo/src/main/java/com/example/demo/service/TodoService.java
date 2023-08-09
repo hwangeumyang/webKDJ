3d72e3f77ec20df49d4350b7b401c6b1456c42c2
hgy <hwangeumyang@gmail.com>
2023-08-10 01:33:55 +0900

    0809 backend in home

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,11 +1,29 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
 
 @Service
 public class TodoService {
 
+    @Autowired
+    TodoRepository repository;
+
     public String testService() {
-        return "Test Service";
+        // create TodoEntity 
+        TodoEntity entity = TodoEntity.builder().title("my first todo item").build();
+        // save TodoEntity
+        repository.save(entity);
+        // retreive TodoEntity 
+        // TodoEntity savedEntity = repository.findById(entity.getId()).get();
+        TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
+
+
+        return savedEntity.getTitle();
     }
+
+    
+
 }
